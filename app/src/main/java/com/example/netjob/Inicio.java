791b1fcf40f982c39a809b3e40c148e4f9310164
santiagofalcon <santiagofalcon12@hotmail.com>
2022-03-18 20:53:48 +0100

    prueba de commit con comentarios

--- app/src/main/java/com/example/netjob/Inicio.java
+++ app/src/main/java/com/example/netjob/Inicio.java
@@ -51,3 +51,5 @@ public class Inicio extends AppCompatActivity {
         startActivity(intent);
     }
 }
+
+//esto es una prueba
--- app/src/main/java/com/example/netjob/Registro.java
+++ app/src/main/java/com/example/netjob/Registro.java
@@ -70,3 +70,5 @@ public class Registro extends AppCompatActivity {
     }
 
 }
+
+

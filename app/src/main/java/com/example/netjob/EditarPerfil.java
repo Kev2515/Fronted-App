2433c2cda555628425922045aa9877c3a9093a2d
Kev2515 <kevs2515@gmail.com>
2022-03-23 20:09:20 +0100

    creadas las pantallas de layout de perfil propio, perfil visitado, editar perfil y crear servicio

---
+++ app/src/main/java/com/example/netjob/EditarPerfil.java
@@ -0,0 +1,28 @@
+package com.example.netjob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class EditarPerfil extends AppCompatActivity {
+
+    TextView localidad;
+    Spinner localidades;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_perfil);
+
+        localidad=findViewById(R.id.editTextTextPersonName7);
+        localidades=findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter =ArrayAdapter.createFromResource(this,R.array.countries, android.R.layout.simple_spinner_item);
+
+        localidades.setAdapter(adapter);
+    }
+}

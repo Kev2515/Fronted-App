8ce97f21dfa36a6809f8c6b441dd9606f3891d22
Alejandro <alex.notario.alcantara@gmail.com>
2022-03-24 20:27:30 +0100

    Creación de la clase Home y su activity_home.xml Creada la clase GridAdapter y grid_categorias.xml

---
+++ app/src/main/java/com/example/netjob/GridAdapter.java
@@ -0,0 +1,57 @@
+package com.example.netjob;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class GridAdapter extends BaseAdapter {
+    Context context;
+    String[] categoriaName;
+    int[] categoriaImage;
+
+    LayoutInflater inflater;
+
+    public GridAdapter(Context context, String[] categoriaName, int[] categoriaImage) {
+        this.context = context;
+        this.categoriaName = categoriaName;
+        this.categoriaImage = categoriaImage;
+    }
+
+    @Override
+    public int getCount() {
+        return categoriaName.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if(inflater == null)
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (convertView == null){
+            convertView = inflater.inflate(R.layout.grid_categorias, null);
+        }
+
+        ImageView imageView = convertView.findViewById(R.id.categoriaImage);
+        TextView textView = convertView.findViewById(R.id.categoriaName);
+
+        imageView.setImageResource(categoriaImage[position]);
+        textView.setText(categoriaName[position]);
+
+        return convertView;
+    }
+}

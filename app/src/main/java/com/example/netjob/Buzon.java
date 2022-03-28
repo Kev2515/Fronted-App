292ec043217be8e86ed0d3d3d1866b4f10b5ff0c
Alejandro <alex.notario.alcantara@gmail.com>
2022-03-28 13:39:29 +0200

    Creadas las actividades de buzón, Lista servicios, y Detalle servicios

---
+++ app/src/main/java/com/example/netjob/Buzon.java
@@ -0,0 +1,14 @@
+package com.example.netjob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Buzon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buzon);
+    }
+}
---
+++ app/src/main/java/com/example/netjob/MisServicios.java
@@ -0,0 +1,14 @@
+package com.example.netjob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MisServicios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mis_servicios);
+    }
+}

7bdd6fe81470233e92686b88cd12bfd11f9cde13
Alejandro Notario <alex.notario.alcantara@gmail.com>
2022-04-06 18:33:36 +0200

    Correción del CategoriaAdapter.java y pormenores

--- app/src/main/java/com/example/netjob/CategoriaAdapter.java
+++ app/src/main/java/com/example/netjob/CategoriaAdapter.java
@@ -1,56 +1,56 @@
 package com.example.netjob;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class CategoriaAdapter extends BaseAdapter {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+
+import com.example.netjob.Model.Categoria;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class CategoriaAdapter extends ArrayAdapter {
+
     Context context;
-    String[] categoriaName;
-    int[] categoriaImage;
+    int itemLayout;
+    List<Categoria> categorias;
 
-    LayoutInflater inflater;
-
-    public CategoriaAdapter(Context context, String[] categoriaName, int[] categoriaImage) {
+    public CategoriaAdapter(@NonNull Context context, int resource, @NonNull List<Categoria> objects) {
+        super(context, resource, objects);
         this.context = context;
-        this.categoriaName = categoriaName;
-        this.categoriaImage = categoriaImage;
+        itemLayout = resource;
+        categorias = objects;
     }
 
+    @NonNull
     @Override
-    public int getCount() {
-        return categoriaName.length;
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-
-        if(inflater == null)
-            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         if (convertView == null){
-            convertView = inflater.inflate(R.layout.categoria, null);
+            LayoutInflater layoutInflater = LayoutInflater.from(context);
+            convertView = layoutInflater.inflate(itemLayout, parent, false);
         }
 
-        ImageView imageView = convertView.findViewById(R.id.categoriaImage);
-        TextView textView = convertView.findViewById(R.id.categoriaName);
 
-        imageView.setImageResource(categoriaImage[position]);
-        textView.setText(categoriaName[position]);
+        TextView name = convertView.findViewById(R.id.categoriaName);
+        name.setText(categorias.get(position).getName());
+
+
+        ImageView image = convertView.findViewById(R.id.categoriaImage);
+        image.setImageResource(categorias.get(position).getImage());
+
 
         return convertView;
     }

43c6fbae62156fc8eede17069c7ade0bb3d8dbba
Kev2515 <kevs2515@gmail.com>
2022-04-08 17:20:22 +0200

    Correciones de la vista

--- app/src/main/java/com/example/netjob/ServicioAdapter.java
+++ app/src/main/java/com/example/netjob/ServicioAdapter.java
@@ -5,8 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -37,11 +37,11 @@ public class ServicioAdapter extends ArrayAdapter {
             convertView = layoutInflater.inflate(itemLayout, parent, false);
         }
 
-        EditText linea1 = convertView.findViewById(R.id.linea1);
+        TextView linea1 = convertView.findViewById(R.id.linea1);
         linea1.setText(servicios.get(position).getLinea1());
 
-        EditText linea2 = convertView.findViewById(R.id.linea2);
-        linea1.setText(servicios.get(position).getLinea2());
+        TextView linea2 = convertView.findViewById(R.id.linea2);
+        linea2.setText(servicios.get(position).getLinea2());
 
 
         ImageView image = convertView.findViewById(R.id.servicioImage);

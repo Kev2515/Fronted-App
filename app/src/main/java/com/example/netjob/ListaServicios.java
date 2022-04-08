a9fe732bedb572b84013579895827b1484c3a34e
Alejandro Notario <alex.notario.alcantara@gmail.com>
2022-04-08 16:41:08 +0200

    Creacion del adapter para ListaServicios.java

--- app/src/main/java/com/example/netjob/ListaServicios.java
+++ app/src/main/java/com/example/netjob/ListaServicios.java
@@ -5,9 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
+import android.widget.Toast;
+import com.example.netjob.Model.Servicio;
+import com.example.netjob.databinding.ActivityListaServiciosBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListaServicios extends AppCompatActivity {
+
+    ActivityListaServiciosBinding binding;
 
     private ImageButton Inicio;
     private ImageButton Buzon;
@@ -18,72 +27,93 @@ public class ListaServicios extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lista_servicios);
-        bindUi();
+        binding = ActivityListaServiciosBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        Inicio.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToInicio();
-            }
-        });
-        Buzon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToBuzon();
-            }
-        });
-        Buscar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToBuscar();
-            }
-        });
-        Favoritos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToFavoritos();
-            }
-        });
-        Perfil.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToPerfil();
-            }
-        });
+        List<Servicio> servicios = new ArrayList<>();
 
-    }
-    private void bindUi() {
+        ServicioAdapter gridAdapter = new ServicioAdapter(ListaServicios.this, R.layout.servicio, servicios);
+        binding.servicioOfrecido.setAdapter(gridAdapter);
 
-        Inicio = findViewById(R.id.imageButton);
-        Buzon = findViewById(R.id.imageButton1);
-        Buscar = findViewById(R.id.imageButton2);
-        Favoritos = findViewById(R.id.imageButton3);
-        Perfil = findViewById(R.id.imageButton4);
-    }
-    private void goToInicio(){
-        Intent intent = new Intent(ListaServicios.this, Home.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-    }
-    private void goToBuzon(){
-        Intent intent = new Intent(ListaServicios.this, Buzon.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-    }
-    private void goToBuscar(){
-        Intent intent = new Intent(ListaServicios.this, ListaServicios.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-    }
-    private void goToFavoritos(){
-        Intent intent = new Intent(ListaServicios.this, Favoritos.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-    }
-    private void goToPerfil(){
-        Intent intent = new Intent(ListaServicios.this, PerfilPropio.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
+        binding.servicioOfrecido.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                bindUi();
+
+                Toast.makeText(ListaServicios.this, "Accediendo a servicio de " + servicios.get(position).getLinea1(), Toast.LENGTH_SHORT).show();
+
+                Inicio.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        goToInicio();
+                    }
+                });
+                Buzon.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        goToBuzon();
+                    }
+                });
+                Buscar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        goToBuscar();
+                    }
+                });
+                Favoritos.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        goToFavoritos();
+                    }
+                });
+                Perfil.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        goToPerfil();
+                    }
+                });
+
+            }
+
+            private void bindUi() {
+
+                Inicio = findViewById(R.id.imageButton);
+                Buzon = findViewById(R.id.imageButton1);
+                Buscar = findViewById(R.id.imageButton2);
+                Favoritos = findViewById(R.id.imageButton3);
+                Perfil = findViewById(R.id.imageButton4);
+            }
+
+            private void goToInicio() {
+                Intent intent = new Intent(ListaServicios.this, Home.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+
+            private void goToBuzon() {
+                Intent intent = new Intent(ListaServicios.this, Buzon.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+
+            private void goToBuscar() {
+                Intent intent = new Intent(ListaServicios.this, ListaServicios.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+
+            private void goToFavoritos() {
+                Intent intent = new Intent(ListaServicios.this, Favoritos.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+
+            private void goToPerfil() {
+                Intent intent = new Intent(ListaServicios.this, PerfilPropio.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
     }
 }
+
